89538337708f4df96c4ac59ad33007cad6749417
Erik Sik <eriksik97@gmail.com>
2023-10-12 13:00:03 +0200

    added log in button temp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,16 @@ import './globals.css'
 
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return <div className={inter.className}>
         <SessionProvider session={pageProps.session}>
+            <Link href={"api/auth/signin"}>
+                Sign In
+            </Link>
             <Component {...pageProps} />
         </SessionProvider>
     </div>
