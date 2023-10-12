1d49918eea35c00423aa2b2c1ff043f4bed454ca
Erik Sik <eriksik97@gmail.com>
2023-10-12 12:52:37 +0200

    Added nextauth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return <div className={inter.className}>
-        <Component {...pageProps} />
+        <SessionProvider session={pageProps.session}>
+            <Component {...pageProps} />
+        </SessionProvider>
     </div>
 }
