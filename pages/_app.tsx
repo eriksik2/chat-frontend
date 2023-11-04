47edcea618cdeaedfd02491ce144aa51e9e3ed89
Erik Sik <eriksik97@gmail.com>
2023-11-04 22:34:50 +0100

    Added keywords and description info in Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import RootLayout from "@/components/Layout/RootLayout";
 
 import { Analytics } from "@vercel/analytics/react";
 import { trpc } from "@/util/trcp";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,19 +17,37 @@ function MyApp({ Component, pageProps }: AppProps) {
     (Component as any).getLayout ?? ((page: JSX.Element) => page);
 
   return (
-    <div
-      className={clsx(
-        inter.className,
-        "relative h-screen w-screen",
-        "bg-white",
-        "dark:text-black",
-      )}
-    >
-      <SessionProvider session={pageProps.session}>
-        <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
-      </SessionProvider>
-      <Analytics />
-    </div>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <title key="title">Chat Labs</title>
+        <meta
+          key="keywords"
+          name="keywords"
+          content={`chat, chatgpt, ai, gpt, gpt3, gpt-3, chatbot, chatbots, gpt4, gpt-4, chatgpt4`}
+        />
+        <meta
+          key="description"
+          name="description"
+          content="Chat Labs lets you create your own AIs, chat with them, and share them."
+        />
+
+        <link rel="canonical" href="https://chatlabs.me/" />
+      </Head>
+      <div
+        className={clsx(
+          inter.className,
+          "relative h-screen w-screen",
+          "bg-white",
+          "dark:text-black",
+        )}
+      >
+        <SessionProvider session={pageProps.session}>
+          <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
+        </SessionProvider>
+        <Analytics />
+      </div>
+    </>
   );
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Chat Labs</title>
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
       <body>
